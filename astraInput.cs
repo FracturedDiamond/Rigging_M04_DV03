199d9dc5adee1a018d5881a4208c72238b31bc6c
FracturedDiamond <49692399+FracturedDiamond@users.noreply.github.com>
2020-11-11 18:50:16 -0700

    Add files via upload

---
+++ astraInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class astraInput : MonoBehaviour
+{
+    private Animator astraAnimator = null;
+    private float verticalPress;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        astraAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalPress = Input.GetAxis("Vertical");
+        astraAnimator.SetFloat("Blend", verticalPress);
+    }
+}
